6a93419ac3d5e94704d98db9aa99ffc7eb7de29c
Bekele Debisa <bdebisa@gmail.com>
2020-08-06 15:14:58 +0300

    successfullu bootstraped the application with needed modules

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-  </div>
+  <hot-table
+    licenseKey="non-commercial-and-evaluation"
+    :data="data"
+    rowHeaders="true"
+    colHeaders="true"
+  ></hot-table>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import { HotTable } from "@handsontable/vue";
 
 export default {
   name: "Home",
+  data: function() {
+    return {
+      data: [
+        ["", "Ford", "Volvo", "Toyota", "Honda"],
+        ["2016", 10, 11, 12, 13],
+        ["2017", 20, 11, 14, 13],
+        ["2018", 30, 15, 12, 13]
+      ]
+    };
+  },
   components: {
-    HelloWorld
+    HotTable
   }
 };
 </script>
+
+<style src="../../node_modules/handsontable/dist/handsontable.full.css"></style>
