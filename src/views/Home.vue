67bb6a1c80e80a57ff200b69e31a72091cbc7c98
Bekele Debisa <bdebisa@gmail.com>
2020-08-07 20:15:01 +0300

    rendered basic table

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,88 @@
 <template>
   <hot-table
     licenseKey="non-commercial-and-evaluation"
-    :data="data"
-    rowHeaders="true"
-    colHeaders="true"
+    :settings="hotSettings"
+    :rowHeaders="true"
+    :colHeaders="true"
   ></hot-table>
 </template>
 
 <script>
 import { HotTable } from "@handsontable/vue";
+// import Handsontable from "handsontable";
 
 export default {
   name: "Home",
   data: function() {
     return {
-      data: [
-        ["", "Ford", "Volvo", "Toyota", "Honda"],
-        ["2016", 10, 11, 12, 13],
-        ["2017", 20, 11, 14, 13],
-        ["2018", 30, 15, 12, 13]
-      ]
+      baseData: [
+        [
+          -1,
+          "Make Div Table",
+          "Noel",
+          true,
+          "In Progress",
+          new Date().toLocaleDateString()
+        ],
+        [
+          0,
+          "Make Insert of the Column",
+          "Patrick",
+          false,
+          "Not Started",
+          new Date(2020, 7, 20).toLocaleDateString()
+        ],
+        [
+          1,
+          "Make components for every type",
+          "John",
+          false,
+          "Completed",
+          new Date().toLocaleDateString()
+        ]
+      ],
+      hotSettings: {}
     };
   },
   components: {
     HotTable
+  },
+  computed: {
+    settings() {
+      let hotData = Array.from({
+        length: 23 - this.baseData.length
+      }).map(() => []);
+      hotData.unshift(...this.baseData);
+      return {
+        data: hotData,
+        minCols: 26,
+        minRows: 26,
+        cellAlignment: "left",
+        colWidth: 150,
+        columns: [
+          {},
+          {},
+          {},
+          { type: "checkbox" },
+          {
+            editor: "select",
+            selectOptions: ["In Progress", "Not Started", "Completed"]
+          },
+          { type: "date" }
+        ],
+        colHeaders: [
+          "State",
+          "Task",
+          "Assigned To",
+          "Reviewed",
+          "Status",
+          "Due"
+        ]
+      };
+    }
+  },
+  created() {
+    this.hotSettings = this.settings;
   }
 };
 </script>
